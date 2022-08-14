9a00f1686c241ae98792b60c23c78a33ba79b627
Kris Shkodrani <kshkodrani@gmail.com>
2022-08-14 16:20:05 +0200

    Declare ENV vars with REACT_APP prefix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import 'dotenv/config';
 
-const BASE_URL = process.env.BACKEND_BASE_URL ? process.env.BACKEND_BASE_URL : 'http://localhost:8000';
+const BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL;
 
 export async function getPizzas() {
     try {
